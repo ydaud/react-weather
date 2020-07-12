4c4d1a99f82a946ce3a95da65a22be3c2c57e27c
Yahya Daud <ydaud@ucsd.edu>
2020-07-11 22:09:55 -0500

    added location and error msg

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,20 +2,35 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { dailyWeather } from '../reducers/weatherReducer'
 
+const LocationCard = ({ city, country }) => {
+  return (
+    <div>
+      <h3>{city}, {country}</h3>
+    </div>
+  )
+}
+
 const WeatherCard = ({ id }) => {
   const dispatch = useDispatch()
   const weather = useSelector(state => state.weather)
 
-  console.log(weather)
-  console.log
+  console.log('Weather', weather)
 
   useEffect(() => {
     dispatch(dailyWeather(id))
   }, [dispatch, id])
 
+  if (weather === null) {
+    return (
+      <div>
+        No such city
+      </div>
+    )
+  }
+
   return (
     <div>
-      Hello
+      <LocationCard city={weather.name} country={weather.sys.country} />
     </div>
   )
 }
